cfc0a97816a5601d2236c8d7cdcf27669d6b5311
jngcii <concotree@gmail.com>
2021-01-31 04:08:14 +0900

    웹팩에 바벨 적용(ts 설정 누락)

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
       // es6 코드를 바벨이 처리하는 것처럼 타입스크립트 코드를 ts-loader가 처리하도록 설정했다. .ts나 .tsx 파일을 ts-loader가 해석하라는 설정
       { test: /\.tsx?$/, loader: "ts-loader" },
 
+      // babel
+      // 실무에서는
+      // npx babel src/app.js 로 바벨을 적용하는 것이 아니라
+      // webpack을 통해 각 모듈들에 babel.config.js 설정을 적용하는 것이 일반적!
+      // 참고로 ts에 대한 고려가 전혀 안된 -> https://babeljs.io/docs/en/babel-preset-typescript 를 참고~~~
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader", // 바벨 로더를 추가한다
+      },
+
       // loader에 배열을 설정하면 뒤의 로더부터 동작한다.
       // 아래 설정은 css-loader를 적용해 css를 js코드로 변환한 후에, style-loader를 적용해 js로 변경된 스타일을 동적으로 돔에 추가
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
